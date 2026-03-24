e8a41a0f2a1aabf9bb5e6155d736df8345e146b7
MerwinJoshwa <joshwamerwin81@gmail.com>
2026-03-24 09:32:33 +0530

    UC2: Add Passenger Bogies to Train

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,17 +1,48 @@
 import java.util.*;
+
+class UseCase2TrainConsistManagementApp{
+    String bog;
+    public void addPassenger(ArrayList<String> list){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter Passenger bogies:");
+        bog=input.nextLine();
+        list.add(bog);
+    }
+
+    public void display(ArrayList<String> list){
+        for(String item: list){
+            System.out.println(item);
+        }
+    }
+
+    public void remove(ArrayList<String> list,int pos){
+        list.remove(pos);
+    }
+}
+
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        UseCase2TrainConsistManagementApp uc2 =new UseCase2TrainConsistManagementApp();
         System.out.println("=====================================");
-        System.out.println(" === Train Consist Management App===");
+        System.out.println(" UC2 - Add Passenger Bogies to Train");
         System.out.println("=====================================\n");
 
-        List<String> trainConsist = new ArrayList<>();
+        ArrayList<String> trainConsist = new ArrayList<>();
+        System.out.println("Enter no of Bogies to add");
+        int n=input.nextInt();
+        for(int i=0;i<n;i++){
+            uc2.addPassenger(trainConsist);
 
-        System.out.println("Train initialized successfully...");
-        System.out.println("Initial Bogie Count : "+trainConsist.size());
-        System.out.println("Current Train Consist : []");
+        }
+        System.out.println("Removing bogies from list");
+        System.out.println("Enter position to remove");
+        int pos=input.nextInt();
+        uc2.remove(trainConsist,pos);
+        uc2.display(trainConsist);
 
-        System.out.println("System ready for operations..");
+
+
 
 
     }
