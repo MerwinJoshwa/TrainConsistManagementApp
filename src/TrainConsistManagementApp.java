0a8cd61f48d0b7277fadf05058eb8bd38c48412d
MerwinJoshwa <joshwamerwin81@gmail.com>
2026-03-31 21:32:45 +0530

    UC4

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,44 +1,24 @@
-import java.util.*;
+import java.util.LinkedList;
 
-class UseCase3TrainConsistManagementApp{
-    String bog;
-    public void addbogieID(HashSet<String> set){
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter bogie ID: ");
-        bog=input.nextLine();
-        set.add(bog);
-    }
-
-    public void display(HashSet<String> set){
-        for(String item: set){
-            System.out.println(item);
-        }
-    }
-
-
-}
-
-public class TrainConsistManagementApp {
+public class  TrainConsistManagementApp{
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        UseCase3TrainConsistManagementApp uc3 =new UseCase3TrainConsistManagementApp();
-        System.out.println("=====================================");
-        System.out.println(" UC3 -  Track Unique Bogie ID");
-        System.out.println("=====================================\n");
+        System.out.println("=== Train Consist Management App ===");
 
-        HashSet<String> bogies=new HashSet<>();
+        LinkedList<String> trainConsist = new LinkedList<>();
 
-        System.out.println("Enter no of Bogies ID to add");
-        int n=input.nextInt();
-        for(int i=0;i<n;i++){
-            uc3.addbogieID(bogies);
-        }
-        uc3.display(bogies);
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
 
+        trainConsist.add(2, "Pantry Car");
 
+        System.out.println("Train consist after insertion: " + trainConsist);
 
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
 
-
-
+        System.out.println("Final ordered train consist: " + trainConsist);
     }
 }
